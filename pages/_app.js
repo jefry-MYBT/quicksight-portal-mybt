761c8dfe4988707fe4704cdee2eddad0780d895b
jefry-MYBT <jefry.maldonado@bodytechcorp.com>
2023-10-26 16:11:59 -0500

    feat: analisis de vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import "@/styles/globals.css";
 import { SessionProvider } from "next-auth/react";
+import { Analytics  } from "@vercel/analytics/react";
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider>
         <Component {...pageProps} />
+        <Analytics />
     </SessionProvider>
   );
 }
